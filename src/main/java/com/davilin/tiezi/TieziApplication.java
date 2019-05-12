d0c6324f733af594283c3e32b30ddf250bea1d1c
DaviLinGH <DaviLinjob@163.com>
2019-05-12 16:54:16 +0800

    first commit

---
+++ src/main/java/com/davilin/tiezi/TieziApplication.java
@@ -0,0 +1,13 @@
+package com.davilin.tiezi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TieziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TieziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davilin/tiezi/TieziApplicationTests.java
@@ -0,0 +1,16 @@
+package com.davilin.tiezi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TieziApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
